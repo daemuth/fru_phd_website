de294d7936749ab7953ab832caab0888793a9c06
Akos <levelameister@gmail.com>
2022-05-30 16:38:22 +0100

    Bugfix: Posts containing > 100 posts now get cached properly
    Also refactored the caching code to be generalised

--- _data/notion.js
+++ _data/notion.js
@@ -2,101 +2,63 @@ const Cache = require("@11ty/eleventy-cache-assets");
 require("dotenv").config();
 
 module.exports = async function () {
-  let notion_posts_db_url = `https://api.notion.com/v1/databases/${process.env.NOTION_POSTS_DB}/query`
-  let notion_pages_db_url = `https://api.notion.com/v1/databases/${process.env.NOTION_PAGES_DB}/query`
-  let notion_cards_db_url = `https://api.notion.com/v1/databases/${process.env.NOTION_CARDS_DB}/query`
 
-  let cacheOptions = {
-    duration: "1s",
-    type: "json",
-    fetchOptions: {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${process.env.NOTION_TOKEN}`,
-        "Notion-Version": "2021-08-16",
-        "Content-Type": "application/json"
-      },
-    }
-  }
+  let notionDatabases = [{'name': 'posts', 'id': `${process.env.NOTION_POSTS_DB}`}, {
+    'name': 'pages', 'id': `${process.env.NOTION_PAGES_DB}`
+  }, {'name': 'cards', 'id': `${process.env.NOTION_CARDS_DB}`}]
 
-  const filter = {
-    "filter": {
-      "property": "Published",
-      "checkbox": {
-        "equals": true
+
+  function getCacheOptions(method) {
+    return {
+      duration: "1d", type: "json", fetchOptions: {
+        method: `${method}`, headers: {
+          "Authorization": `Bearer ${process.env.NOTION_TOKEN}`,
+          "Notion-Version": "2021-08-16",
+          "Content-Type": "application/json"
+        },
       }
     }
   }
 
-  let posts = await Cache(notion_posts_db_url, cacheOptions)
-  let pages = await Cache(notion_pages_db_url, cacheOptions)
-  let cards = await Cache(notion_cards_db_url, cacheOptions)
+  let dataNew = {}
 
-  for (var post of posts.results) {
-    let post_url = 'https://api.notion.com/v1/blocks/' + post.id + '/children?page_size=100'
-    post.content = await Cache(post_url, {
-      duration: "1s",
-      type: "json",
-      fetchOptions: {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${process.env.NOTION_TOKEN}`,
-          "Notion-Version": "2021-08-16",
-          "Content-Type": "application/json"
-        },
+  for (let database of notionDatabases) {
+    let currentDatabaseUrl = `https://api.notion.com/v1/databases/${database.id}/query`
+
+    // List object of all pages within database
+    let currentDatabaseCached = await Cache(currentDatabaseUrl, getCacheOptions("POST"))
+
+    //Array of objects containing all pages
+    for (let page of currentDatabaseCached.results) {
+
+      if ('properties' in page && 'Name' in page.properties) {
+        page.title = page.properties.Name.title[0].plain_text
       }
-    })
+
+      page.content = []
+      await cacheNotionPageBlocks(page, page, page.id)
+    }
+
+    dataNew[database.name] = currentDatabaseCached.results
   }
 
-  for (var page of pages.results) {
-    let page_url = 'https://api.notion.com/v1/blocks/' + page.id + '/children?page_size=100'
-    page.content = await Cache(page_url, {
-      duration: "1s",
-      type: "json",
-      fetchOptions: {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${process.env.NOTION_TOKEN}`,
-          "Notion-Version": "2021-08-16",
-          "Content-Type": "application/json"
-        },
-      }
-    })
+  async function cacheNotionPageBlocks(originalPage, parentPage, parent_id, start_cursor) {
+
+    //Do not put cursor for first query
+    let page_url = typeof start_cursor === 'undefined' ? `https://api.notion.com/v1/blocks/${parent_id}/children?page_size=100` : `https://api.notion.com/v1/blocks/${parent_id}/children?page_size=100&start_cursor=${start_cursor}`
+
+    var page = await Cache(page_url, getCacheOptions("GET"))
+
+    for (let block of page.results) {
+      originalPage.content.push(block)
+    }
+
+    if (page.has_more === true) {
+      let next_cursor = page['next_cursor']
+      let nextPageUrl = `https://api.notion.com/v1/blocks/${parent_id}/children?start_cursor=${next_cursor}&page_size=100`
+      let nextPage = await Cache(nextPageUrl, getCacheOptions("GET"))
+      await cacheNotionPageBlocks(originalPage, nextPage, parent_id, next_cursor)
+    }
   }
-
-  for (var card of cards.results) {
-    let card_url = 'https://api.notion.com/v1/blocks/' + card.id + '/children?page_size=100'
-    card.content = await Cache(card_url, {
-      duration: "1s",
-      type: "json",
-      fetchOptions: {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${process.env.NOTION_TOKEN}`,
-          "Notion-Version": "2021-08-16",
-          "Content-Type": "application/json"
-        },
-      }
-    })
-  }
-
-  posts = posts.results
-  pages = pages.results
-  cards = cards.results
-
-  for (const post of posts) {
-    post.title = post.properties.Name.title[0].plain_text;
-  }
-
-  for (const page of pages) {
-    page.title = page.properties.Name.title[0].plain_text;
-  }
-
-  let data = {
-    posts: posts,
-    pages: pages,
-    cards: cards
-  }
-
-  return data
+  return dataNew
 };
